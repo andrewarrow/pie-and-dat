79ea1909cb4f31e73288ebf934d0732d1f8ea6a9
Andrew Arrow <andrew@bird.co>
2019-02-19 04:41:39 +0000

    end

--- example/dir.go
+++ example/dir.go
@@ -7,19 +7,17 @@ import "io/ioutil"
 import "github.com/andrewarrow/pie-and-dat"
 
 var m = map[string]string{}
+var counts = map[string]int{}
 var i = 1
 
 func processFileByCategory(name, category string) {
-	if category == "17.json" {
+	if category != "17.json" {
 		p := readAFileFullOfJson(name)
 		pie.FindKeys(p.Data, "channelId", "channelTitle")
 		for _, v := range pie.Pair {
 			m[v["id"]] = v["name"]
+			counts[v["id"]]++
 		}
-	}
-	for k, v := range m {
-		fmt.Println(i, k, v)
-		i++
 	}
 }
 
@@ -49,6 +47,12 @@ func LoadADirFullOfJsonFiles(path string) {
 			assignMeaning(name, i == 3, t, "hour")
 			assignMeaning(name, i == 4, t, "minute")
 			assignMeaning(name, i == 6, t, "category")
+		}
+	}
+	for k, v := range m {
+		if counts[k] > 4 {
+			fmt.Println(i, k, v, counts[k])
+			i++
 		}
 	}
 
